01e7df9664a3026d840b5fd065947f4f1d6e0c09
lcy2 <lcy2@l.com>
2017-06-19 18:20:05 -0400

    added a failsafe for the curtain drops

--- splitter/static/splitter/splitter-loading-screen.js
+++ splitter/static/splitter/splitter-loading-screen.js
@@ -1,7 +1,15 @@
-var slidUp = false;
 function slide_up(){
   $('.dot1, .dot2').addClass('dots_disappear');
   $('#loading').addClass('moved');
   $('#loading_msg').addClass('dots_disappear');
   $("body, html").css({"overflow":"initial"});
 }
+
+$(document).ready(function(){
+  // set maximum time curtain is down
+  setTimeout(function(){
+    if (!$('#loading').hasClass('moved')){
+      slide_up();
+    }
+  }, 2000);
+})
